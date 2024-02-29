8c0b408042a5fff681d6e93b71a4e6f21ece38f9
gabgilp <gabgilp@gmail.com>
2024-02-29 13:08:09 +0100

    changed how the values of cards are set
    changed to specific functions for setting the values of the cards so that we have flexibility when generating them rather than having to set their values all at once, also for example to add a sentence or a meaning later on.

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -10,8 +10,10 @@ public class App {
 
     public static void main(String[] args) {
         try { // try catch is only necessary because we are throwing an exception, if we overload the constructor with default values, we can remove the try catch
-            Card card = new Card("hello", "hola", "greeting", "saludo");
-            System.out.println(card.fromWord);
+            Card card = new Card();
+            card.setFromWord("hello");
+            card.setToWord("hola");
+            System.out.println(card.toWord);
         } catch (Exception e) {
             e.printStackTrace();
         }
